d5c5f1d4f133e1e26b6d18f7bc12ee10ad526041
Ravi Kumar Narnolia <87347904+RaviKumarNarnolia@users.noreply.github.com>
2023-01-06 14:14:59 +0530

    Update let_var_const.js

--- let_var_const.js
+++ let_var_const.js
@@ -1,3 +1,6 @@
 // Var is globally scoped while let and cosnt are block scoped
 //var can be updated and can be re-declared within its scope
 //whereas let can be updated and cannot be re-declared 
+//const can neither be updated nor be re-declared
+
+console.log("Creating first file of le var and const") 
