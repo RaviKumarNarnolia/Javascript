b7b575d9ead73abce64bc294266a03e0bb40ea92
Ravi Kumar Narnolia <87347904+RaviKumarNarnolia@users.noreply.github.com>
2023-01-06 14:34:17 +0530

    Update let_var_const.js

--- let_var_const.js
+++ let_var_const.js
@@ -4,3 +4,35 @@
 //const can neither be updated nor be re-declared
 
 console.log("Creating first file of le var and const") 
+
+var a = 45
+var b = "Ravi"
+var c = 12
+{
+    var b = "this"
+    console.log(b)
+}
+console.log(b)
+
+let a = 45
+let b = "Ravi"
+let c = 12
+{
+    let b = "this"
+    console.log(b)
+}
+console.log(b)
+
+var b = 45
+var b = "Ravi"
+let c = 12
+
+console.log(b)
+
+console.log("Creating first file of let, var and const") 
+
+const author = "Ravi"
+//let author = "Ravi"   // this will throw an error because const variable cannot be changed and re-declared
+let c = 12
+
+console.log(author)
